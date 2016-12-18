77da71dbf19de0ca3b69b600218cbe0dd1a5ca8a
arnauld dumez <arnauld.dumez@gmail.com>
2016-12-18 09:32:14 +0100

    new

--- Deploy-Database.ps1
+++ Deploy-Database.ps1
@@ -5,8 +5,17 @@ $scriptPath = $args[2]
 Add-Type -Path (Join-Path -Path (Get-Item -Path ".\" -Verbose).FullName -ChildPath 'DbUp.dll')
 
 $dbUp = [DbUp.DeployChanges]::To
-$dbUp = [SqlServerExtensions]::SqlDatabase($dbUp, "server=.;database=KbcList;Trusted_Connection=Yes;Connection Timeout=120;")
+$dbUp = [SqlServerExtensions]::SqlDatabase($dbUp, "server=$env:serverName;database=KbcList;Trusted_Connection=Yes;Connection Timeout=120;")
 $dbUp = [StandardExtensions]::WithScriptsFromFileSystem($dbUp, "C:\Work\KanbanCore\scripts\files")
 $dbUp = [SqlServerExtensions]::JournalToSqlTable($dbUp, 'dbo', 'SchemaVersion')
 $dbUp = [StandardExtensions]::LogToConsole($dbUp)
-$upgradeResult = $dbUp.Build().PerformUpgrade()
+$result = $dbUp.Build().PerformUpgrade()
+if (!$result.Successful) {
+    $errorMessage = ""
+    if ($result.Error -ne $null) {
+        $errorMessage = $result.Error.Message
+    }
+    Write-Host "##vso[task.logissue type=error;]Database migration failed. $errorMessage"
+    Write-Host "##vso[task.complete result=Failed;]"
+    Exit -1
+}
